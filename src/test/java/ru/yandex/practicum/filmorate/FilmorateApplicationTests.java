42d83b875430f4b91237e3fd027d6073017922d8
KyleKiske <d44i7ryr@yandex.ru>
2022-12-17 06:09:04 +0300

    В тест добавлена реализация встроенной для тестов базы данных, чтобы отделить её от основной.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,18 +3,20 @@ package ru.yandex.practicum.filmorate;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.dao.DBUserRepository;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @SpringBootTest
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
 	private final DBUserRepository userRepository;
@@ -22,12 +24,18 @@ class FilmorateApplicationTests {
 	@Test
 	public void testFindUserById() {
 
+		userRepository.createUser(User.builder()
+						.login("test")
+						.name("TestName")
+						.email("test@testmail.tv")
+						.birthday(LocalDate.parse("1965-01-03"))
+				.build());
 		Optional<User> userOptional = userRepository.getUserById(1L);
 
 		assertThat(userOptional)
 				.isPresent()
 				.hasValueSatisfying(user ->
-						assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+						assertThat(user).hasFieldOrPropertyWithValue("id", 1L)
 				);
 	}
 }
